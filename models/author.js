0ff5406614f630cb3fa6f6c23ee64711efccbed8
Benjamin-Albarzendji <105798796+Benjamin-Albarzendji@users.noreply.github.com>
2023-07-13 21:05:11 +0200

    New views

--- models/author.js
+++ models/author.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
+const { DateTime } = require('luxon');
 
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
@@ -25,6 +26,15 @@ AuthorSchema.virtual('name').get(function () {
 AuthorSchema.virtual('url').get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+});
+
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export model
